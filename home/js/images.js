5c7ede2b3269ccef56544620c92bc9bfb3d5e04f
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-01-10 08:39:17 +0800

    Create images.js

---
+++ home/js/images.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+   data() {
+    return {
+      manyFoods: [
+        {name: 'Burrito', url: 'img_burrito.svg'},
+        {name: 'Salad', url: 'img_salad.svg'},
+        {name: 'Cake', url: 'img_cake.svg'},
+        {name: 'Soup', url: 'img_soup.svg'},
+        {name: 'Fish', url: 'img_fish.svg'},
+        {name: 'Pizza', url: 'img_pizza.svg'},
+        {name: 'Rice', url: 'img_rice.svg'}
+      ]
+    }
+   }
+  })
+  
+  app.mount('#app')
