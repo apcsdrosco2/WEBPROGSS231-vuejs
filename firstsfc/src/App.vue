ab7a53ec2e67fe60fea729aeb2d742ce6b3c2593
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-01-27 22:57:09 +0800

    push

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <h1>Food</h1>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
-  <food-item2/>
   <personal-profile/>
-
+  <food-item/>
+  <food-item2/>
+  <food-item/>
 </template>
 
 <script></script>
