da012365e2b35eebd7924f36175d4d6f496b671a
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-02-02 05:57:53 +0000

    yuh

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+  <script setup>
   import { ref, onMounted } from 'vue'
   import { supabase } from './lib/supabaseClient'
 
@@ -12,21 +12,10 @@
   onMounted(() => {
     getCountries()
   })
-</script>
+  </script>
 
-
-<template>
-  <ul>
-  <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-
-  <h1>Food</h1>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <personal-profile/> <br>
-
-</template>
-
-
-<style></style>
+  <template>
+    <ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    </ul>
+  </template>
