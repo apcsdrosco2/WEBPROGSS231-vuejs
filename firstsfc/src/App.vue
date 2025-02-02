cc5955ec23413bd54954f27916aa6e21ef242c29
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-02-02 06:02:07 +0000

    waaaaaaa

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,36 +1,12 @@
-<script setup>
-  import { ref, onMounted } from 'vue';
-  import { supabase } from './lib/supabaseClient';
-
-  const countries = ref([]);
-
-  async function getCountries() {
-    const { data, error } = await supabase.from('countries').select();
-    if (error) {
-      console.error('Error fetching countries:', error);
-    } else {
-      console.log('Fetched countries:', data);
-      countries.value = data;
-    }
-  }
-
-  onMounted(() => {
-    getCountries();
-  });
-</script>
+<script></script>
 
 
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
-
   <h1>Food</h1>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
   <personal-profile/> <br>
-
 </template>
 
 
