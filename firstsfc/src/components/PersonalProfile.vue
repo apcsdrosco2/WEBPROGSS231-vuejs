e6a0e3f85c4df0d501abc579db93ddf1bd5ae353
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-02-02 05:56:25 +0000

    fix?

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -5,7 +5,7 @@
         <div>4 IT Experience</div>
         <div>5 Hobbies & Interests</div>
         <div>6 Goals in Life / Dream</div>
-        <div>7 Piclure Gallery</div>
+        <div>7 Picture Gallery</div>
 </template>
 
 <script></script>
