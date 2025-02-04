1e955c3819bda82bbe034286c1dcffe75623d590
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-02-04 09:34:31 +0800

    push

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -18,13 +18,13 @@
 <template>
   
   <h1>Food</h1>
+  <country/>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
   <personal-profile/> <br>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li> <br>
-  </ul>
+  <comment-form/>
+  <comment/>
 </template>
 
 
