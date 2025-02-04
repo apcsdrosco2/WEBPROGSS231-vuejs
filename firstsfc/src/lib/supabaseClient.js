fb18aa9e287f6f0e44eebc91ce4d7bc778de3f10
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-02-04 18:43:17 +0800

    yuh

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://rduihiqxzggscvyaafrm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkdWloaXF4emdnc2N2eWFhZnJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NzA0NDMsImV4cCI6MjA1NDA0NjQ0M30._q0fNOWBJBwo7wp2wtcyUco0bDPorhjTiQZKntOkzT8')
+export const supabase = createClient('https://dikzynlokhvyulyvsjdz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpa3p5bmxva2h2eXVseXZzamR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NzExMzYsImV4cCI6MjA1NDA0NzEzNn0.tl_ED-c1QTFL3uDS3ZNsFsIgctVf0dYhRKCWfNtdEiU')
